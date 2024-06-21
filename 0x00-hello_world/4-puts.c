6fda21c1b6556f8d532a9042abc7fa8e50b9a514
IlyasElt <ilyasseelattar@gmail.com>
2024-06-21 07:50:54 -0700

    Hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,21 @@
 /*
  * File: 4-puts.c
  * Author: Ilyas Elattar
- * Description: this file contains a function that prints this following line:
- * "Programming is like building a multilingual puzzle
+ * Description: This file contains a function that prints this following line:
+ * "Programming is like building a multilingual puzzle"
  */
-
 
 #include <stdio.h>
 
-
+/**
+ * main - Entry point
+ * Description: This function prints the following line:
+ * "Programming is like building a multilingual puzzle"
+ * Return: 0 if the program completes successfully
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
