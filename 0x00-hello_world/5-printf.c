7d86806c18a9c4acbe9450517579aed77b0f6b58
IlyasElt <ilyasseelattar@gmail.com>
2024-06-21 08:03:17 -0700

    Add: C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+/*
+ * File: 5-printf.c
+ * Author: Ilyas Elattar
+ * Description: this file contains a function that prints exactly:
+ * with proper grammar, but the outcome is a piece of art,
+ */
+
+#include <sdio.h>
+
+/**
+ * main - Entry point
+ * Description: this function prints exactly:
+ * with proper grammar, but the outcome is a piece of art,
+ * Return: 0 if the program completes successfully
+ */
+
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
