d2a9db0a48f0f2b0fb22d7deae96f8c9634acf03
IlyasElt <ilyasseelattar@gmail.com>
2024-06-21 07:32:45 -0700

    Add: C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/*
+ * File: 4-puts.c
+ * Author: Ilyas Elattar
+ * Description: this file contains a function that prints this following line:
+ * "Programming is like building a multilingual puzzle
+ */
+
+
+#include <stdio.h>
+
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
