3cc136a5eda7372da9d496e8271dc4fadbdebf14
IlyasElt <ilyasseelattar@gmail.com>
2024-06-26 05:01:05 +0200

    Add: function that checks if an argument is alphabetic.

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isalpha - A function that checks if an argument is alphabetic.
+ * @c: the argument to check if its alphabetic.
+ * Return: 1 if the argument is alphabetic and 0 if it's not.
+ */
+
+int _isalpha(int c);
+{
+if (isalpha(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
