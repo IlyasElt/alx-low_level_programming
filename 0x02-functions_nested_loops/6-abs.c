ae6fe152745e23f6c358c2215ed3acdd6930a689
IlyasElt <ilyasseelattar@gmail.com>
2024-06-26 05:32:35 +0200

    Add: Function that returns the absolute value of a number.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - Function that returns the absolute value of a number.
+ * @n: The number that the functions works around.
+ * Return: the absolute value of a number.
+ */
+
+int _abs(int n)
+{
+return (abs(n));
+}
