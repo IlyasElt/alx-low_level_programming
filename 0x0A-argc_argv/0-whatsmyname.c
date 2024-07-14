31947e3cb051f10658165e62d362871554f85824
IlyasElt <ilyasseelattar@gmail.com>
2024-07-14 14:16:29 +0100

    Oops! forgot the betty coding style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
 /**
- * 
+ * main - program that prints its name, followed by a new line
+ * @argc: count of command line arguments
+ * @argv: array of strings passed to the command line arguments
+ *
+ * Return: 0
  */
 
-int main(int __attribute__((unused)) argc, char* argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
-    printf("%s\n", argv[0]);
+printf("%s\n", argv[0]);
 
-    return (0);
+return (0);
 }
