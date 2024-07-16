0d21f28009ef55cd633982d4e386c640ea72227f
IlyasElt <ilyasseelattar@gmail.com>
2024-07-16 21:10:31 +0100

    Add: function taht concatenates two strings.

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * str_concat - Concatenates two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: A pointer to the newly allocated space in memory containing
+ * the concatenated string, or NULL if memory allocation fails.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+int i, j;
+int length = 0;
+char *string;
+
+if (s1 == NULL || s2 == NULL)
+{
+string = "  \0";
+return (string);
+}
+
+for (i = 0; s1[i] != '\0'; i++)
+{
+length++;
+}
+for (i = 0; s2[i] != '\0'; i++)
+{
+length++;
+}
+
+string = malloc(length + 1 * sizeof(char));
+if (string == NULL)
+{
+return (NULL);
+}
+
+for (i = 0; s1[i] != '\0'; i++)
+{
+string[i] = s1[i];
+}
+for (j = 0; s2[j] != '\0'; i++, j++)
+{
+string[i] = s2[j];
+}
+
+string[i] = '\0';
+
+return (string);
+}
