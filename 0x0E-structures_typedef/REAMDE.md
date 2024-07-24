72273f4ab44303174fcebaa0a8aa561f0d7c5145
IlyasElt <ilyasseelattar@gmail.com>
2024-07-24 20:39:54 +0100

    Exploting the concepts of structs and typedef in C.

---
+++ 0x0E-structures_typedef/REAMDE.md
@@ -0,0 +1,3 @@
+Answering these following questions:
+- What are structures, when, why and how to use them
+- How to use typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Represents a dog with a few attributes
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
