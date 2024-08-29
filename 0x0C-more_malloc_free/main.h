a53e8c62bcedf7cf31d885faaadd9f9866eac771
IlyasElt <ilyasseelattar@gmail.com>
2024-08-29 20:41:04 +0100

    Fix

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,8 +9,6 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-char *argstostr(int ac, char **av);
-void ch_free_grid(char **grid, unsigned int height);
-char **strtow(char *str);
+
 
 #endif /* MAIN_H */
